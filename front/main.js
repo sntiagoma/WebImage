e98a10ea3cefaa344b05e09b79d0ec947bd9f165
Santiago <sntiagoma@gmail.com>
2016-02-13 18:32:55 -0500

    Adding Angular's CDN

--- front/main.js
+++ front/main.js
@@ -1,4 +1,4 @@
-var angular = require("angular");
+//var angular = require("angular"); //If don't have internet to try angular
 var app = angular.module("WebImage",[]);
 
 app.controller('WebImageController', ['$scope', function($scope){
